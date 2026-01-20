2136be5d3dd6b4410d19919ee6f6fe95c5d5184a
sharmabhumika41634 <sharmabhumika41634@gmail.com>
2026-01-20 15:23:15 +0530

    Added my class pricatic file

---
+++ Class-practice/Voting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+  public class Voting{
+  public Statis void main(String[] agrs){
+  Scanner Sc = new Scanner(System.in);
+  
+  System.out.println("Enter to your age: ");
+  int age = Sc.nextint();
+  
+  if(age >= 18){
+  System.out.println("you are Eligible for voting.");
+  }else{
+  System.out.println("your not Eligible for voting.");
+  }
+  }
+  }
