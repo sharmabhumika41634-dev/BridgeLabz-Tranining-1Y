1a035bbbe17f2e7a7fc937f4f936ae1e51d4740d
sharmabhumika41634 <sharmabhumika41634@gmail.com>
2026-01-28 09:58:46 +0530

    Added my java lop program

---
+++ Class-practice/DoWhile.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+   class DoWhile {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+	int i =1;
+    do{
+	System.out.println(i+ " ");
+	i++;
+	}
+	while(i<=20);
+	}
+	}
+	
---
+++ Class-practice/ForEachLoop.java
@@ -0,0 +1,10 @@
+ public class ForEachloop {
+    public static void main(String[] args) {
+
+        int[] a = {10, 20, 30, 40, 50};
+
+        for (int x : a) {
+            System.out.println(x);
+        }
+    }
+}
